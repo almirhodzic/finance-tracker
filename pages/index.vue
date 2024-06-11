36d514c64ffa9ae68aadd2028baca2b57c3d493e
almirh <24458972+almirhodzic@users.noreply.github.com>
2024-06-11 10:23:58 +0200

    Update Trend Options & Dynamic Amounts

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-
   <section class="flex items-center justify-between mb-10">
     <h1 class="text-3xl font-extrabold">
       Summary
@@ -8,25 +7,41 @@
       <USelectMenu class="w-32" v-model="selectedView" :options="transactionViewOptions" />
     </div>
   </section>
-
   <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
-    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="isLoading" />
-    <Trend color="red" title="Expense" :amount="4000" :last-amount="5000" :loading="isLoading" />
+    <Trend color="green" title="Income" :amount="incomeTotal" :last-amount="3000" :loading="isLoading" />
+    <Trend color="red" title="Expense" :amount="expenseTotal" :last-amount="5000" :loading="isLoading" />
     <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="isLoading" />
     <Trend color="red" title="Saving" :amount="4000" :last-amount="4100" :loading="isLoading" />
   </section>
-
+  <section class="flex justify-between mb-10">
+    <div>
+      <h2 class="text-2xl font-extrabold">Transactions</h2>
+      <div class="text-gray-500 dark:text-gray-400">
+        You have {{ incomeCount }} income transactions and {{ expenseCount }} expenses this period
+      </div>
+    </div>
+    <div>
+      <UModal v-model="isOpen">
+      <UCard>
+        <template #header>
+          Add Transaction
+        </template>
+        <div>Hello!</div>
+      </UCard>
+    </UModal>
+      <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" @click="isOpen = true" />
+    </div>
+  </section>
   <section v-if="!isLoading">
     <div v-for="(transactionsOnDay, date) in transactionsGroupedByDate" :key="date" class="mb-10">
       <DailyTransactionSummary :date="date" :transactions="transactionsOnDay" />
       <Transaction v-for="transaction in transactionsOnDay" :key="transaction.id" :transaction="transaction" 
       @deleted="refsreshTransactions()" />
     </div>
-  </section>
+  </section>  
   <section v-if="isLoading">
     <USkeleton class="h-8 w-full mb-2" v-for="i in 4" :key="i" />
   </section>
-
 </template>
 
 <script setup>
@@ -35,6 +50,24 @@ const supabase = useSupabaseClient()
 const selectedView = ref(transactionViewOptions[1])
 const transactions = ref([])
 const isLoading = ref(false)
+const isOpen = ref(false)
+const income = computed(
+  () => transactions.value.filter(t => t.type === 'Income')
+)
+const expense = computed(
+  () => transactions.value.filter(t => t.type === 'Expense')
+)
+
+const incomeCount = computed(() => income.value.length)
+const expenseCount = computed(() => expense.value.length)
+
+const incomeTotal = computed(
+  () => income.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
+
+const expenseTotal = computed(
+  () => expense.value.reduce((sum, transaction) => sum + transaction.amount, 0)
+)
 
 const fetchTransactions = async () => {
   isLoading.value = true
